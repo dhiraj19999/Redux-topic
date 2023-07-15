f2361906d24e64abb5977bee97ccb76827863b50
dhiraj19999 <yogeshsamse682@gmail.com>
2023-07-15 10:27:47 +0530

    Add

--- Day2/my-app/src/redux/store.js
+++ Day2/my-app/src/redux/store.js
@@ -1,3 +1,5 @@
-import { legacy_createStore } from "redux";
+import { legacy_createStore,compose} from "redux";
 import { reducer } from "./reducer";
-export const store = legacy_createStore (reducer);
+
+const composeEnhanser=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+export const store = legacy_createStore (reducer,composeEnhanser());
